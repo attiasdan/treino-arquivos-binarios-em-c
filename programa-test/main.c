0382fe29226343419b4023e927521fa4d69c3dc5
Daniel Attias <danielattias@live.com>
2022-07-18 19:32:26 -0400

    'Update'

--- programa-test/main.c
+++ programa-test/main.c
@@ -1,25 +1,23 @@
 /*
 Arq 4 - indices_clientes:
-    campo 'Ã­ndice' â€“ inteiro
-    campo 'posiÃ§Ã£o que o registro ocupa (inteiro)' â€“ inteiro
+    campo 'índice' – inteiro
+    campo 'posição que o registro ocupa (inteiro)' – inteiro
 
 Arq 5 - indices_produtos:
-    campo 'Ã­ndice' â€“ inteiro
-    campo 'posiÃ§Ã£o que o registro ocupa (inteiro)' â€“ inteiro
+    campo 'índice' – inteiro
+    campo 'posição que o registro ocupa (inteiro)' – inteiro
 
 Arq 6 - indices_vendas:
-    campo 'Ã­ndice' â€“ inteiro
-    campo 'posiÃ§Ã£o que o registro ocupa (inteiro)' â€“ inteiro
+    campo 'índice' – inteiro
+    campo 'posição que o registro ocupa (inteiro)' – inteiro
 */
 
-//F6 para compilar no vscode
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
 #include <time.h>
-//#include "funcoes.c"
 
 #define MAX_CLIENTE 100
 #define MAX_PRODUTO 100
@@ -40,7 +38,7 @@ typedef struct {
 } Produto;
 
 typedef struct {
-    int nroVenda; //índice
+    int nroVenda; //ï¿½ndice
     char dataVendas[10]; //ok
     
     int codCliente; //ok
@@ -56,19 +54,59 @@ Venda vendas[MAX_VENDA];
 
 void menu();
 void cadastrarVenda();
-//void ordenar(); //ordenação bolha
-//void consultar(); //busca binária
+//void ordenar(); //ordenaï¿½ï¿½o bolha
+//void consultar(); //busca binï¿½ria
 
 int main()
 {
-	system("color f0");
-	system("title Trabalho de Arquivos Binários - Disciplina Programação 1");
+	system("color f0"); //mudança nas cores do terminal
+	system("title Trabalho de Arquivos Binarios - Disciplina Programacao 1"); //definicao do titulo da janela
     
 	menu();
 
     return 0;
 }
-
+int abrirArquivoLeitura(char arquivo[20]) {
+	FILE *fp;
+	
+	fp = fopen(arquivo, "rb");
+	
+	if (!fp) {
+		printf("Não foi possível abrir o arquivo para leitura");
+		return;
+	}
+	fclose(fp);
+}
+int abrirArquivoEscrita(char arquivo[20]) {
+	FILE *fp;
+	
+	fp = fopen(arquivo, "wb");
+	
+	if (!fp) {
+		printf("Nao foi possivel abrir o arquivo para escrita");
+	}
+	fclose(fp);
+}
+void printarTodoArquivo(char arquivo[20])
+{
+	FILE *fp;
+	
+	fp = fopen(arquivo, "rb");
+	
+	if (!fp) {
+		printf("Nao foi possivel abrir o arquivo para leitura");
+	}
+	
+	Cliente auxCliente;
+	
+	while ( fread(&auxCliente, sizeof(char), 1, arquivo) ) {
+		printf("%d", auxCliente.id);
+		printf("%s", auxCliente.nome);
+		printf("%s", auxCliente.telefone);
+		printf("%s", auxCliente.endereco);
+	}
+	fclose(fp);
+}
 void menu()
 {
     system("cls");
@@ -77,19 +115,19 @@ void menu()
     do {
     	printf("Bem vindo(a)!\n\nSelecione uma opcao do menu, digitando o numero e, logo em seguida, pressione a tecla ENTER\n\n");
         printf("1 - Cadastrar 1(um) produto\n");
-        printf("2 - Excluir 1(um) produto	\n");
+        printf("2 - Excluir 1(um) produto\n");
         printf("3 - Consultar 1(um) produto\n");
-        printf("4 - Editar 1(um) produto	\n\n");
+        printf("4 - Editar 1(um) produto\n\n");
         
 		printf("5 - Cadastrar 1(um) cliente\n");
-        printf("6 - Excluir 1(um) cliente	\n");
+        printf("6 - Excluir 1(um) cliente\n");
         printf("7 - Consultar 1(um) cliente\n");
-        printf("8 - Editar 1(um) cliente	\n\n");
+        printf("8 - Editar 1(um) cliente\n\n");
         
-		printf("9 - Cadastrar 1(uma) venda	\n");
-        printf("10 - Excluir 1(uma) venda	\n");
-        printf("11 - Consultar 1(uma) venda	\n");
-        printf("12 - Editar 1(uma) venda	\n\n");
+		printf("9 - Cadastrar 1(uma) venda\n");
+        printf("10 - Excluir 1(uma) venda\n");
+        printf("11 - Consultar 1(uma) venda\n");
+        printf("12 - Editar 1(uma) venda\n\n");
     	
         printf("0 - FECHAR PROGRAMA\n\n");
         
@@ -138,47 +176,47 @@ void cadastrarVenda()
 
 }//cadastrarVenda
 
-void consultar(int codigo) { //busca por código
+void consultar(int codigo) { //busca por cï¿½digo
 	
 }
 void inserir() {
-	//solicitar do usuário o código
-		//verificar se o código já existe no respectivo arquivo de índice
-			//fazer pesquisa binária no próprio arquivo:
+	//solicitar do usuï¿½rio o cï¿½digo
+		//verificar se o cï¿½digo jï¿½ existe no respectivo arquivo de ï¿½ndice
+			//fazer pesquisa binï¿½ria no prï¿½prio arquivo:
 				//obter o tamanho do arquivo (atributos.c)
 				//obter o registro do meio
-				//pesquisar chave até encontrar código ou finalizar a pesquisa
-		//caso já exista o código, informar o código que já foi cadastrado.
-		//não existindo o código:
-			//solicitar ao usuário as demais informações
+				//pesquisar chave atï¿½ encontrar cï¿½digo ou finalizar a pesquisa
+		//caso jï¿½ exista o cï¿½digo, informar o cï¿½digo que jï¿½ foi cadastrado.
+		//nï¿½o existindo o cï¿½digo:
+			//solicitar ao usuï¿½rio as demais informaï¿½ï¿½es
 			//inserir no final do arquivo de dados respectivo
-			//inserir no arquivo de índice informando o código e a posição no arquivo de dados
+			//inserir no arquivo de ï¿½ndice informando o cï¿½digo e a posiï¿½ï¿½o no arquivo de dados
 				//inserir no final do arquivo
-				//usar o método da bolha para ordenação do arquivo de índice
+				//usar o mï¿½todo da bolha para ordenaï¿½ï¿½o do arquivo de ï¿½ndice
 	
 	int codigo = 0;
-	printf("Entre com o código do registro:\n> ");
+	printf("Entre com o cï¿½digo do registro:\n> ");
 	scanf("%d", &codigo);
 }
 void atualizar() {
-//alteração de registro
-		//solicitar do usuário o código
-		//verificar se o mesmo já existe no respectivo arquivo de índice
-		//caso não exista, informar ao usuário;
-		//existindo o código:
+//alteraï¿½ï¿½o de registro
+		//solicitar do usuï¿½rio o cï¿½digo
+		//verificar se o mesmo jï¿½ existe no respectivo arquivo de ï¿½ndice
+		//caso nï¿½o exista, informar ao usuï¿½rio;
+		//existindo o cï¿½digo:
 			//mostrar os valores dos campos;
-			//perguntar qual campo alterar (só pode um por vez)
-			//pedir a nova informação e alterar no arquivo de dados.
+			//perguntar qual campo alterar (sï¿½ pode um por vez)
+			//pedir a nova informaï¿½ï¿½o e alterar no arquivo de dados.
 }
 void excluir() {
 /*
 Excluir registro:
-- Pedir primeiro o código;
-- Verificar se o mesmo já existe no respectivo arquivo de índice;
-- Caso não exista, informar ao usuário;
-- Existindo o código:
-- pedir confirmação de exclusão
+- Pedir primeiro o cï¿½digo;
+- Verificar se o mesmo jï¿½ existe no respectivo arquivo de ï¿½ndice;
+- Caso nï¿½o exista, informar ao usuï¿½rio;
+- Existindo o cï¿½digo:
+- pedir confirmaï¿½ï¿½o de exclusï¿½o
 - excluir o registro do arquivo de dados (vamos discutir em sala)
-- recriar o arquivo de índice (vamos discutir em sala)
+- recriar o arquivo de ï¿½ndice (vamos discutir em sala)
 */
 }
